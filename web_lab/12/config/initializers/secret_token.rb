0d7c819e40fb6dd246c9f25d208d42e2e7ad622a
kpadma <kpadma2@gmail.com>
2013-11-01 22:24:40 +0530

    Initial commit

---
+++ web_lab/12/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Books::Application.config.secret_token = 'cbadbb6d78b46ba0081e4ec035e0e422b244cb2ab2555a57dcd2b5541e56bea0f2909e044a997c05fba005cfb0e9211917b8656d23a9ed9c63dc091c112b4502'
